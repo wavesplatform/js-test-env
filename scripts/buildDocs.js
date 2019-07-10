12db9bb81c3927b57ef08984640cac04037fd5fc
siem <siemarell@gmail.com>
2019-07-10 10:01:29 +0300

    fix docs

--- scripts/buildDocs.js
+++ scripts/buildDocs.js
@@ -6,19 +6,19 @@ const {rmdir} = require('./utils');
 
 const mainIn = './src/augmentedGlobal.d.ts';
 const interfacesIn = './node_modules/@waves/waves-transactions/dist/transactions.d.ts';
-const mainOut = './build/global.d.ts'
-const interfacesOut = './build/transactions.d.ts';
+// const mainOut = './build/global.d.ts'
+// const interfacesOut = './build//node_modules/@waves/waves-transactions/dist/transactions.d.ts';
 const typedocPath = './node_modules/typedoc/bin/typedoc';
-
-if (!fs.existsSync('build')) {
-    fs.mkdirSync('build');
-    // rmdir('build')
-}
+//
+// if (!fs.existsSync('build')) {
+//     fs.mkdirSync('build');
+//     // rmdir('build')
+// }
 
 
 /// Prepare main file
-const mainContent = fs.readFileSync(mainIn, "utf8").replace('declare global {', '').trim().slice(0,-1);
-fs.writeFileSync(mainOut, mainContent, "utf-8");
+// const mainContent = fs.readFileSync(mainIn, "utf8").replace('declare global {', '').trim().slice(0,-1);
+// fs.writeFileSync(mainOut, mainContent, "utf-8");
 
 /// Prepare interface file
 const modulePrefix = '/**\n' +
@@ -26,11 +26,11 @@ const modulePrefix = '/**\n' +
     ' */\n';
 const interfacesContent = fs.readFileSync(interfacesIn, "utf8");
 const firstImportIndex = interfacesContent.indexOf('import');
-fs.writeFileSync(interfacesOut, modulePrefix, "utf-8");
-fs.appendFileSync(interfacesOut, interfacesContent.slice(firstImportIndex), "utf-8")
+fs.writeFileSync(interfacesIn, modulePrefix, "utf-8");
+fs.appendFileSync(interfacesIn, interfacesContent.slice(firstImportIndex), "utf-8")
 
 
-execSync(`${typedocPath} --listFiles ${interfacesOut} ${mainOut}`);
+execSync(`${typedocPath} --listFiles ${interfacesIn} ${mainIn}`);
 console.log('Docs built successfully');
 
 
