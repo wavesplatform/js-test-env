43d1bcba286049ba527bc5655d52acf48a22895a
siem <siemarell@gmail.com>
2019-10-28 17:34:59 +0300

    done

--- scripts/copyPackageJson.js
+++ scripts/copyPackageJson.js
@@ -4,5 +4,6 @@ const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 packageJson.main = 'index.js'
 packageJson.types = 'index.d.ts'
+packageJson.scripts = {}
 
 fs.writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import augment from './augment';
-
+const augment = require('./augment').default;
+let global;
+try {
+    global = Function('return this')();
+} catch (e) {
+    global = window;
+}
 augment(global);
-(global as any).env = {
+global.env = {
     API_BASE: 'https://nodes-testnet.wavesnodes.com',
     SEED: 'default seed',
-    CHAIN_ID: 'T'
+    CHAIN_ID: 'T',
+    timeout: 20000
 };
