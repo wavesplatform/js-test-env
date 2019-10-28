e62cd61a661e1a589728e7dd79e4057be39afa21
siem <siemarell@gmail.com>
2019-10-28 18:24:05 +0300

    :bug: fix jest name clashing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const augment = require('./augment').default;
-let global;
+let globalObj;
 try {
-    global = Function('return this')();
+    globalObj = Function('return this')();
 } catch (e) {
-    global = window;
+    globalObj = window;
 }
-augment(global);
-global.env = {
+augment(globalObj);
+globalObj.env = {
     API_BASE: 'https://nodes-testnet.wavesnodes.com',
     SEED: 'default seed',
     CHAIN_ID: 'T',
